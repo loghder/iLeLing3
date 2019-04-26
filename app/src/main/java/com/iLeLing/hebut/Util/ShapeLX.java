fd41eb8de45e6ffc7385c93c60f0c914166cf7ed
blozh <1165884639@qq.com>
2019-04-27 07:58:25 +0800

    轻薄的假相

---
+++ app/src/main/java/com/iLeLing/hebut/Util/ShapeLX.java
@@ -0,0 +1,41 @@
+package com.iLeLing.hebut.Util;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+import com.iLeLing.hebut.R;
+
+public class ShapeLX extends ConstraintLayout {
+    ShapeLX(Context context){
+        super(context);
+        LayoutInflater.from(context).inflate(R.layout.shape_layout, this, true);
+
+        t1=findViewById(R.id.titleLX);
+        t2=findViewById(R.id.numLX);
+    }
+
+    ShapeLX(Context context, AttributeSet attrs){
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.shape_layout, this, true);
+        t1=findViewById(R.id.titleLX);
+        t2=findViewById(R.id.numLX);
+    }
+
+    ShapeLX(Context context,String title,String num){
+        super(context);
+        LayoutInflater.from(context).inflate(R.layout.shape_layout, this, true);
+
+    }
+
+    public void setTextInf(String title,String num){
+        t1.setText(title);
+        t2.setText(num);
+    }
+
+    TextView t1,t2;
+    ConstraintLayout constraintLayout;
+
+}
--- app/src/main/java/com/iLeLing/hebut/fragment/FourFragment.java
+++ app/src/main/java/com/iLeLing/hebut/fragment/FourFragment.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.iLeLing.hebut.R;
+import com.iLeLing.hebut.Util.ShapeLX;
+import com.qmuiteam.qmui.util.QMUIViewHelper;
 
 public class FourFragment extends Fragment {
 
@@ -20,12 +22,16 @@ public class FourFragment extends Fragment {
     // Content View Elements
     private android.support.constraint.ConstraintLayout mFrameLayout;
     private com.qmuiteam.qmui.widget.QMUITopBar mTopbar_four;
+    private ShapeLX shapeLX01,shapeLX02,shapeLX03;
 
     // End Of Content View Elements
     private void bindViews() {
         Activity activity=getActivity();
         mFrameLayout = (android.support.constraint.ConstraintLayout) activity.findViewById(R.id.frameLayout);
         mTopbar_four = (com.qmuiteam.qmui.widget.QMUITopBar) activity.findViewById(R.id.Topbar_four);
+        shapeLX01=activity.findViewById(R.id.shapeLX01);
+        shapeLX02=activity.findViewById(R.id.shapeLX02);
+        shapeLX03=activity.findViewById(R.id.shapeLX03);
 
     }
 
@@ -42,6 +48,15 @@ public class FourFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         bindViews();
-        mTopbar_four.setTitle("第四页");
+        mTopbar_four.setTitle("");
+        mTopbar_four.addRightImageButton(R.drawable.icon_list, QMUIViewHelper.generateViewId()).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        shapeLX01.setTextInf("累计接单","21");
+        shapeLX02.setTextInf("进行中","3");
+        shapeLX03.setTextInf("已完成","18");
     }
 }
